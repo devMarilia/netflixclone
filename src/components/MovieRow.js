b73e323dad9d8f3dc1e6fcd396392929ed92d0a3
devMarilia <devMarilia.frontend@gmail.com>
2020-10-28 10:25:49 -0300

    Static, criado componente MovieRow e importado dentro do App.js

---
+++ src/components/MovieRow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MovieRow.css';
+// import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+// import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+export default function MovieRow({title, items}){
+
+    return(
+        <div className="movieRow">
+            <h2>Componente MovieRow</h2>
+        </div>
+    );
+}
