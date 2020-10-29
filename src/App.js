d51862fa0eb8a27b4bcda16f6e97dcde3cbbe0cd
devMarilia <devMarilia.frontend@gmail.com>
2020-10-28 21:12:58 -0300

    Statc-inicio da estilização component Featured.css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import './App.css'
 import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
 export default function App() {
 
   const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
 
 useEffect(() => {
   const loadAll = async () => {
     //Pegando a lista total
     let list = await Tmdb.getHomeList();
     setMovieList(list)
+
+    //Pegando a Fetuared
+    let originals = list.filter(i=> i.slug === 'originals')
+    let randonChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+    let chosen = originals[0].items.results[randonChosen]
+    let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
   }
 
   loadAll()
@@ -20,11 +29,16 @@ useEffect(() => {
 
  return (
   <div className="page">
-    <div className="listas">
+
+    {featuredData && 
+      <FeaturedMovie item={featuredData}/>
+    }
+
+    <section className="listas">
       {movieList.map((item, key) => (
         <MovieRow key={key} title={item.title} items={item.items}/>
       ))}
-    </div>
+    </section>
   </div>
  );
 }
--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './MovieRow.css'
+import './FeaturedMovie.css'
 
-export default function FeaturedMovie() {
+export default function FeaturedMovie({item}) {
  return (
-   <div>
-       Filme em destaque!
-   </div>
+   <section className="featured" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+   }}>
+       <div className="featured-verdical">
+          <div className="featured-horizontal">
+
+          </div>
+       </div>
+   </section>
  );
 }
