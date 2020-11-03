b578999733cad4fbe67d667b1b1145dfc2714d9d
devMarilia <devMarilia.frontend@gmail.com>
2020-11-03 17:43:16 -0300

    Novo component Header e estilização e funcionalidade

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './FeaturedMovie.css'
 
 export default function FeaturedMovie({item}) {
+   console.log(item)
+   let firstDate = new Date(item.first_air_date)
+   
+   let genres = [];
+   for(let i in item.genres){
+      genres.push(item.genres[i].name);
+   }
+
  return (
    <section className="featured" style={{
       backgroundSize: 'cover',
@@ -10,7 +18,18 @@ export default function FeaturedMovie({item}) {
    }}>
        <div className="featured-verdical">
           <div className="featured-horizontal">
-
+            <div className="featured-name">{item.original_name}</div>
+               <div className="featured-info">
+                  <div className="featured-points">{item.vote_average} pontos</div>
+                  <div className="featured-year">{firstDate.getFullYear()}</div>
+                  <div className="featured-seasons">{item.number_of_seasons} temporada{item.number_of_seasons > 1 ? "s" : ''}</div>
+               </div>
+               <div className="featured-description">{item.overview}</div>
+                  <div className="featured-buttons">
+                     <a href={`/watch/${item.id}`} className="featured-watchbutton">► Assistir</a>
+                     <a href={`/list/add/${item.id}`} className="featured-mylistbutton">+ Minha Lista</a>
+                  </div>
+            <div className="featured-genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
           </div>
        </div>
    </section>
